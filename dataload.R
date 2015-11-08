be9812855cde4efd6b7188c5faf30c30830654e1
Fernando Monera <fmonera@opensistemas.com>
2015-11-09 00:35:50 +0100

    Added comments to explain dataload.

--- dataload.R
+++ dataload.R
@@ -1,10 +1,12 @@
 library(sqldf)
 
+# We will need to read the data to generate each plot, so it is better to create a function.
 read_file<- function() {
   data <- read.csv.sql("household_power_consumption.txt", "SELECT * FROM file WHERE Date='1/2/2007' OR Date='2/2/2007'", 
                        header = TRUE, sep = ";", eol="\n", colClasses = c("character", "character"))
+  # Avoid warnings about leaving opened connections with read.csv.sql function
   closeAllConnections()
-  # Just one date/time field
+  # We just need one date/time field
   data$Date <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
   data
 }
